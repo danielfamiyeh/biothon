93e4b148ae84cd997032036150a980063d5b1213
Daniel <daniel.kwaku.famiyeh@gmail.com>
2020-09-14 16:32:13 +0200

    False alarm (I think).

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,10 @@
 import unittest
 from src.seq import *
 
-
 class TestSeq(unittest.TestCase):
     def testConstructor(self):
         sequence = "ATTGCTGTCG"
-        self.assertEqual(sequence, Seq(sequence))
+        self.assertEqual(sequence, Seq(sequence).seq)
 
 
 if __name__ == '__main__':
